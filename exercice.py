d8a57ff3eec37caf1561f6cb73eb658ae142b16a
safaikhlef <71021589+safaikhlef@users.noreply.github.com>
2020-09-09 11:09:57 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,9 +4,11 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        
-        resultat += lettre
-    return mot
+        nombre = 0
+        nombre = ord(lettre)
+        nombre -= 32
+        resultat += chr(nombre)
+    return resultat
 
 
 if __name__ == '__main__':
